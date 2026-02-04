58122e795058643bb0b8cf060ea8ac78ab58b7a6
Tsvetan Todorov Todorov <111016955+TsvetanTodorovTodorov@users.noreply.github.com>
2026-02-04 08:58:09 +0200

    Add files via upload

---
+++ 02-Greater-Number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int numA;
+    cin >> numA;
+    int numB;
+    cin >> numB;
+
+    if (numA > numB)
+    {
+        cout << numA << endl;
+    } else
+    {
+        cout << numB << endl;
+    }
+    
+
+    return 0;
+}
